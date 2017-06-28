9d7d6762f38e7036089897b560dedddee988a2b4
xujunxijc <360499805@qq.com>
2017-06-28 18:53:45 +0800

    Add files via upload

---
+++ xujunxi/Assets/Scripts/UserData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData {
+    private int levelNum = 2;
+    private int[] levelPoint;
+    
+    public void setLevelPoint(int p, int l)
+    {
+        levelPoint[l] = p;
+        PlayerPrefs.SetInt("LEVEL" + l.ToString(), p);
+    }
+    public int getLevelPoint(int l)
+    {
+        return levelPoint[l];
+    }
+    4i
+    public UserData()
+    {
+        levelPoint = new int[levelNum];
+        for (int i = 0; i < levelNum; i++)
+        {
+            levelPoint[i] = PlayerPrefs.GetInt("LEVEL" + i.ToString(), 0);
+        }
+    }
+}
